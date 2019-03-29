cd33e5c5c7de82837b4bde6405ea8817389098be
Nisha Gode <nishagode@gmail.com>
2019-03-29 13:46:24 -0400

    fixed about formatting

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react';
-import { View, Image, Text, ScrollView } from 'react-native';
-import styles from './About.style';
-import Icon from '@expo/vector-icons/FontAwesome';
-import data from '../../utils/Data-raw';
-
+import React, { Component } from "react";
+import { View, Image, Text, ScrollView } from "react-native";
+import styles from "./About.style";
+import Icon from "@expo/vector-icons/FontAwesome";
+import data from "../../utils/Data-raw";
 
 class AboutScreen extends Component {
-    static navigationOptions = {
-        title: 'About',
-    };
+  static navigationOptions = {
+    title: "About"
+  };
   render() {
     return (
       <ScrollView>
         <Image
-          source={require('../../../assets/dashcon.jpg')}
+          source={require("../../../assets/dashcon.jpg")}
           style={styles.banner}
         />
         <View style={styles.container}>
           <Text style={styles.title}>{data.name}</Text>
           <View style={styles.info}>
-            <Icon name="calendar">
-              <Text>{data.date}</Text>
-            </Icon>
-            <Icon name="map-pin">
-              <Text>{data.location.city}</Text>
-            </Icon>
+            <Icon name="calendar" style={styles.icon} />
+            <Text style={styles.text}>{data.date}</Text>
+            <Icon name="map-pin" style={styles.icon} />
+            <Text>{data.location.city}</Text>
           </View>
-          < Text style = {styles.description} >{data.description}</Text>
-          </View>
+          <Text style={styles.description}>{data.description}</Text>
+        </View>
       </ScrollView>
     );
   }
--- src/components/About/About.style.js
+++ src/components/About/About.style.js
@@ -1,37 +1,41 @@
 import { Dimensions } from "react-native";
 
-const { width } = Dimensions.get('window');
+const { width } = Dimensions.get("window");
 
-export default {  
-
-        container: {
-            flex: 1,
-            padding: 20
-        },
-        title: {
-            fontSize: 32,
-            fontWeight: 'bold',
-            justifyContent: 'center',
-            alignItems: 'center',
-        },
-        description: {
-            marginTop: 10,
-            textAlign: 'justify',
-            fontSize: 19,
-        },
-        info: {
-            flex: 1,
-            marginTop: 7,
-            alignItems: 'flex-start',
-            flexDirection: 'row',
-        },
-        name: {
-            fontWeight: 'bold',
-            fontSize: 19,
-        },
-        banner: {
-            flex: 1,
-            height: 300,
-            width
-        },
-    };
+export default {
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  description: {
+    marginTop: 10,
+    fontSize: 19
+  },
+  info: {
+    flex: 1,
+    marginTop: 7,
+    alignItems: "flex-start",
+    flexDirection: "row"
+  },
+  name: {
+    fontWeight: "bold",
+    fontSize: 19
+  },
+  banner: {
+    flex: 1,
+    height: 300,
+    width
+  },
+  icon: {
+    marginRight: 10
+  },
+  text: {
+    marginRight: 75
+  }
+};
