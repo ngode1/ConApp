581976bc6bc8e34db06681a7bcefa3234ad3aab2
Nisha Gode <nishagode@gmail.com>
2019-10-20 10:27:55 -0400

    added horizontal separator

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
-import { Container, Icon } from "native-base";
+import { Container, Icon, View } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
 import HLNCard from "./EventCardHLN";
 import TPPCard from "./EventCardTPP";
 
-export default class Events extends Component {
-  render() {
+export default class Events extends Component
+{
+  render ()
+  {
     return (
       <Container>
         <HLNCard />
+        <View
+          style={ {
+            borderBottomColor: 'black',
+            borderBottomWidth: 2,
+          } }
+        />
         <TPPCard />
       </Container>
     );
