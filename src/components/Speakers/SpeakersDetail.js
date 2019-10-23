7031119a35b766f5338fccce16c1c44bf50c88e8
Nisha Gode <nishagode@gmail.com>
2019-10-23 08:57:52 -0400

    added scrollview

--- src/components/Speakers/SpeakersDetail.js
+++ src/components/Speakers/SpeakersDetail.js
@@ -1,25 +1,34 @@
 import React, { Component } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import styles from "./SpeakersDetailStyle";
 import Icon from "@expo/vector-icons/FontAwesome";
+// import { TwitterFollowButton } from 'react-twitter-embed';
+// import { Follow } from 'react-twitter-widgets';
 
-export default class SpeakersDetail extends Component {
+export default class SpeakersDetail extends Component
+{
   static navigationOptions = {
     title: "Speaker Detail"
   };
 
-  render() {
+  render ()
+  {
     const { name, avatar, bio, twitter } = this.props.navigation.state.params;
     return (
-      <View style={styles.container}>
-        <Image source={{ uri: avatar }} style={styles.photo} />
-        <Text style={styles.name}>{name}</Text>
-        <Text>{bio}</Text>
-        <View style={styles.info}>
-          <Icon style={styles.icon} name="twitter" />
-          <Text>{twitter}</Text>
+      <ScrollView>
+        <View style={ styles.container }>
+          <Image source={ { uri: avatar } } style={ styles.photo } />
+          <Text style={ styles.name }>{ name }</Text>
+          <Text>{ bio }</Text>
+          <View style={ styles.info }>
+            <Icon style={ styles.icon } name="twitter" />
+            <Text>{ twitter }</Text>
+          </View>
+          <View>
+            {/* <Follow username="lost_hufflpuff" /> */ }
+          </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
