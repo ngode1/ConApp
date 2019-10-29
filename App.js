1c1524ee5024c13a623febe1b55e2a5ac22ce893
Nisha Gode <nishagode@gmail.com>
2019-10-29 11:42:04 -0400

    font for Android devices

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ export default class App extends React.Component
   async componentWillMount ()
   {
     await Font.loadAsync( {
-      // Roboto: require( "native-base/Fonts/Roboto.ttf" ),
-      // Roboto_medium: require( "native-base/Fonts/Roboto_medium.ttf" ),
+      Roboto: require( "native-base/Fonts/Roboto.ttf" ),
+      Roboto_medium: require( "native-base/Fonts/Roboto_medium.ttf" ),
       Ionicons: require( "@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf" ),
     } );
   }
